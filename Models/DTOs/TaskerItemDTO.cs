726df18644479e48f3af95ef4d288b86a488ee78
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-05-16 07:20:54 -0500

    Implemented data model and dto

---
+++ Models/DTOs/TaskerItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIWithAuth.Models.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for TaskerItem. 
+    /// Used by the front end and API endpoints.
+    /// </summary>
+    public class TaskerItemDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please choose a name. Every task must be named.")]
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; } = false;
+    }
+}
---
+++ Models/TaskerItem.cs
@@ -0,0 +1,24 @@
+﻿
+//defines properties of a single task item(object) in the tasker list
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPIWithAuth.Models
+{
+    public class TaskerItem
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Please choose a name. Every task must be named.")]
+        public required string Name { get; set; } 
+        public bool IsComplete { get; set; } = false;
+
+        /// <summary>
+        /// Navigation property to the user who created this task.
+        /// defines the relationship between the task and the user--this table's foreign key
+        /// </summary>
+        public virtual IdentityUser User { get; set; } = null!;
+
+    }
+}
