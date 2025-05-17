e57eb85f3daf17f6c25c6c55ac9176ae48778ae3
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-05-17 09:19:28 -0500

    Enabled API Authentication and CORS policy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
 using WebAPIWithAuth.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,14 +15,34 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-    
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    //added authentication to swagger
+    opts =>
+    {
+        opts.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = HeaderNames.Authorization,
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+        });
+    });
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(opts => opts.SignIn.RequireConfirmedAccount = false)
+                                                                                .AddRoles<IdentityRole>()
+                                                                                .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddCors(opts => opts.AddDefaultPolicy(policy =>
+policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
 
 var app = builder.Build();
+
+app.UseCors();
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
